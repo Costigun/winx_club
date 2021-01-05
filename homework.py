2d3d5a3cec55f4e67f60a3d3481b0d3865f85f70
Costigun <maximneveraa@gmail.com>
2021-01-05 18:15:22 +0600

    фыв

--- homework.py
+++ homework.py
@@ -1,4 +1,9 @@
+list1 = [10,20,30,40]
+list2 = []
 i = 0
-while i ** 2 < 10:
-    print(i**2)
-    i+=1
+while i < len(list1):
+    list2.insert(0,list1[i])
+    i = i + 1
+    print(list2)
+print(list1)
+
